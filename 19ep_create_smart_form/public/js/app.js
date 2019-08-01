5e8ea6e73519d2129a906612f2da59a436632d53
Henrique Omena <88lmao@gmail.com>
2019-08-01 22:47:56 +0100

    Done Validation

--- 19ep_create_smart_form/public/js/app.js
+++ 19ep_create_smart_form/public/js/app.js
@@ -1,44 +1,32 @@
 class Errors {
 
     constructor() {
-        this.errorValidacao = {};
+        this.errors = {};
     }
 
     has(field) {
-        console.log('o field', field);
-
-        if (this.errorValidacao.errors) {
-            console.log('o error', this.errorValidacao.errors);
-            console.log('no if', this.errorValidacao.errors.hasOwnProperty(field));
-            console.log('in no if', field in this.errorValidacao.errors)
-            return this.errorValidacao.hasOwnProperty(field);
-        }
-
-        return this.errorValidacao.hasOwnProperty(field);
+        return this.errors.hasOwnProperty(field);
     }
 
     any() {
 
-        return Object.keys(this.errorValidacao).length > 0;
+        return Object.keys(this.errors).length > 0;
 
     }
 
     get(field) {
-        if (this.errorValidacao[field]) {
-            console.log('field no get', field);
-            return this.errorValidacao[field][0];
+        if (this.errors[field]) {
+            return this.errors[field][0];
         }
     }
 
     //only to update ur errors object
     record(errors) {
-        console.log('error no record', errors);
-        this.errorValidacao = errors;
-        console.log('dentro de record', errors);
+        this.errors = errors.errors;
     }
 
     clear(field) {
-        delete this.errorValidacao[field];
+        delete this.errors[field];
     }
 }
 
@@ -56,8 +44,7 @@ new Vue({
             axios.post('/projects', this.$data)
                 .then(response => alert('passou no then de sucesso'))
                 .catch(error => {
-                    console.log('catch do pedido', error.response.data)
-                    return this.errorValidacao.record(error.response.data)
+                    this.errors.record(error.response.data)
                 })
         }
     }
